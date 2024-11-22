3892fbd97548ef6184db3cc7b39ea4a59248ac28
Aygul <gulya@gmx.net>
2024-11-22 15:30:29 +0100

    test: add unit and integration tests for getAllOrganizations

--- backend/src/main/java/de/neuefische/backend/controller/OrganizationController.java
+++ backend/src/main/java/de/neuefische/backend/controller/OrganizationController.java
@@ -3,6 +3,7 @@ package de.neuefische.backend.controller;
 import de.neuefische.backend.model.Organization;
 import de.neuefische.backend.service.OrganizationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,12 @@ public class OrganizationController {
     private final OrganizationService organizationService;
 
     @GetMapping
-    List<Organization> getAllOrganizations() {
-        return organizationService.getAllOrganizations();
+    public ResponseEntity<List<Organization>> getAllOrganizations() {
+        List<Organization> organizations = organizationService.getAllOrganizations();
+        if (organizations.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(organizations);
+
     }
 }

a9c316e53ab4e1effd0b7e467a1bdbba663240ae
Aygul <gulya@gmx.net>
2024-11-27 10:13:13 +0100

    feat: implement handleValidationExceptions

--- backend/src/main/java/de/neuefische/backend/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/de/neuefische/backend/exception/GlobalExceptionHandler.java
@@ -1,9 +1,12 @@
 package de.neuefische.backend.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 
 @RestControllerAdvice
@@ -14,5 +17,21 @@ public class GlobalExceptionHandler {
     public ErrorMessage handleException(Exception exception) {
         return new ErrorMessage("An unexpected error occurred: " + exception.getMessage());
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorMessage handleValidationExceptions(MethodArgumentNotValidException exception) {
+
+        List<String> errorMessages = exception.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(fieldError -> fieldError.getField() + ": " + fieldError.getDefaultMessage())
+                .toList();
+
+        String errorMessage = String.join(", ", errorMessages);
+
+        return new ErrorMessage("Validation failed: " + errorMessage);
+    }
+
 
 }

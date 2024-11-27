90febf2a2ef07aed27a59606c84453bb757ce9b6
Aygul <gulya@gmx.net>
2024-11-27 11:39:26 +0100

    test: add tests for IdService

---
+++ backend/src/test/java/de/neuefische/backend/service/IdServiceTest.java
@@ -0,0 +1,23 @@
+package de.neuefische.backend.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class IdServiceTest {
+    private final IdService idService = new IdService();
+
+    @Test
+    void generateRandomId_shouldReturnValidUUID() {
+        String generatedId = idService.generateRandomId();
+        assertTrue(generatedId.matches("^[a-f0-9-]{36}$"));
+    }
+
+    @Test
+    void generateRandomId_shouldGenerateUniqueIds() {
+        String id1 = idService.generateRandomId();
+        String id2 = idService.generateRandomId();
+        assertNotEquals(id1, id2);
+    }
+}

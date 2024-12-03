f17340d4cdf8e2c77b3ff60230efc33345549fe5
Aygul <gulya@gmx.net>
2024-12-03 11:23:08 +0100

    feat: add search functionality for organization in Home

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -2,6 +2,7 @@ import {Organization} from "../types/Organization.ts";
 import OrganizationGallery from "./OrganizationGallery.tsx";
 import "../styles/Home.css"
 import {useNavigate} from "react-router-dom";
+import {useState} from "react";
 
 type HomeProps = {
     organizations: Organization[]
@@ -11,14 +12,33 @@ function Home(props: HomeProps) {
 
     const navigate = useNavigate();
 
+    const [searchText, setSearchText] = useState("");
+
     const handleAddOrganization = () => {
         navigate("add-organization")
     }
+
+    const filteredOrganizations: Organization[] = props.organizations
+        .filter((organization) => organization.name.toLowerCase().includes(searchText.toLowerCase()));
+
     return (
         <>
             <button onClick={handleAddOrganization}>Weiterbildungsanbieter hinzufügen</button>
             <div className="home-container">
-                <OrganizationGallery organizations={props.organizations}/>
+                <input
+                    type="text"
+                    onChange={(e) => setSearchText(e.target.value)}
+                    placeholder="Nach einem Weiterbildungsanbieter suchen"
+                />
+
+                {filteredOrganizations.length > 0 ? (
+                    <OrganizationGallery organizations={filteredOrganizations}/>
+                ) : (
+                    "Keine Weiterbildungsanbieter gefunden"
+                )}
+                <button className="add" onClick={handleAddOrganization}>
+                    + NEU
+                </button>
             </div>
         </>
     );

a348a1e05ff6c27e0be50f4de905dff1b73fd657
Aygul <gulya@gmx.net>
2024-12-05 14:45:28 +0100

    feat: load multiple pages from external api - add ApiResponsePage to ApiResponse - implement getNextPageUrl method in ApiService - implement equals() and hashCode() in Organization record based on name

--- backend/src/main/java/de/neuefische/backend/api/dto/ApiResponse.java
+++ backend/src/main/java/de/neuefische/backend/api/dto/ApiResponse.java
@@ -3,7 +3,6 @@ package de.neuefische.backend.api.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record ApiResponse(
-        @JsonProperty("_embedded")
-        ApiResponseContent responseContent
+        @JsonProperty("_embedded") ApiResponseContent responseContent, ApiResponsePage page
 ) {
 }
--- backend/src/main/java/de/neuefische/backend/api/service/ArbeitsagenturApiService.java
+++ backend/src/main/java/de/neuefische/backend/api/service/ArbeitsagenturApiService.java
@@ -7,6 +7,7 @@ import de.neuefische.backend.service.IdService;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,32 +17,43 @@ public class ArbeitsagenturApiService {
     private final RestClient restClient;
 
     public ArbeitsagenturApiService(RestClient.Builder builder) {
-        this.restClient = builder.baseUrl("https://rest.arbeitsagentur.de/infosysbub/wbsuche/pc/v2/bildungsangebot")
-                .build();
+        this.restClient =
+                builder.baseUrl("https://rest.arbeitsagentur.de/infosysbub/wbsuche/pc/v2/bildungsangebot").build();
     }
 
-    private static List<Organization> getOrganizations(List<ApiResponseOrganization> apiResponseOrganizations) {
+    public List<Organization> loadAllOrganizations() {
+        List<Organization> apiOrganizations = new ArrayList<>();
+        String urlPage = "?page=0&size=20";
+
+        while (urlPage != null) {
+            Optional<ApiResponse> optionalResponse =
+                    Optional.ofNullable(restClient.get().uri(urlPage).header("X-API" + "-Key", "infosysbub-wbsuche").retrieve().body(ApiResponse.class));
+            ApiResponse response = optionalResponse.orElse(null);
+            urlPage = getNextPageUrl(response);
+
+            List<ApiResponseDetails> details =
+                    optionalResponse.map(ApiResponse::responseContent).map(ApiResponseContent::details).orElseThrow(ApiResponseException::new);
+
+            List<ApiResponseOrganization> apiResponseOrganizations =
+                    details.stream().map(ApiResponseDetails::courseOffer).map(ApiResponseCourseOffer::apiResponseOrganization).distinct().toList();
+            apiOrganizations.addAll(getOrganizations(apiResponseOrganizations));
+
+        }
+        return apiOrganizations.stream().distinct().toList();
+
+    }
+
+    private List<Organization> getOrganizations(List<ApiResponseOrganization> apiResponseOrganizations) {
         IdService idService = new IdService();
         return apiResponseOrganizations.stream().map(a -> new Organization(idService.generateRandomId(), a.name(),
                 a.homepage(), a.email(),
                 a.address().addressDetails().postalCode() + " " + a.address().addressDetails().city() + ", " + a.address().streetAndHomeNumber())).toList();
     }
 
-    public List<Organization> loadAllOrganizations() {
-        String url = "?page=0&size=20";
-        Optional<ApiResponse> optionalResponse = Optional.ofNullable(
-                restClient.get().uri(url)
-                        .header("X-API-Key", "infosysbub-wbsuche")
-                        .retrieve()
-                        .body(ApiResponse.class));
-
-        List<ApiResponseDetails> details = optionalResponse.map(ApiResponse::responseContent)
-                .map(ApiResponseContent::details)
-                .orElseThrow(ApiResponseException::new);
-
-        List<ApiResponseOrganization> apiResponseOrganizations = details.stream().map(ApiResponseDetails::courseOffer)
-                .map(ApiResponseCourseOffer::apiResponseOrganization).distinct().toList();
-        return getOrganizations(apiResponseOrganizations);
-
+    private String getNextPageUrl(ApiResponse apiResponse) {
+        if (apiResponse != null && apiResponse.page() != null && apiResponse.page().number() < 10) {
+            return "?page=" + (apiResponse.page().number() + 1) + "&size=20";
+        }
+        return null;
     }
 }

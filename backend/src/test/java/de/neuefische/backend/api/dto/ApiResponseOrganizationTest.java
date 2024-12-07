d3b13eb66db036447fe66ed4ab56e27aed04405e
Aygul <gulya@gmx.net>
2024-12-07 15:24:11 +0100

    refactor: ensure ApiResponseOrganization equality based on id, update tests

--- backend/src/test/java/de/neuefische/backend/api/dto/ApiResponseOrganizationTest.java
+++ backend/src/test/java/de/neuefische/backend/api/dto/ApiResponseOrganizationTest.java
@@ -8,21 +8,22 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 class ApiResponseOrganizationTest {
 
     @Test
-    void equals_shouldReturnTrueForEqualNames() {
+    void equals_shouldReturnTrueForEqualIds() {
         long id = 123;
-        ApiResponseOrganization apiResponseOrganization1 = new ApiResponseOrganization(id, "testname", "testhomepage"
+        ApiResponseOrganization apiResponseOrganization1 = new ApiResponseOrganization(id, "testname1", "testhomepage"
                 , "testmail", new ApiResponseAddress("testStreet", new ApiResponseAddressDetails("11", "Berlin")));
-        ApiResponseOrganization apiResponseOrganization2 = new ApiResponseOrganization(id, "testname", "testhomepage2"
+        ApiResponseOrganization apiResponseOrganization2 = new ApiResponseOrganization(id, "testname2", "testhomepage2"
                 , "testmail2", new ApiResponseAddress("testStreet2", new ApiResponseAddressDetails("112", "Hamburg")));
         assertEquals(apiResponseOrganization1, apiResponseOrganization2);
     }
 
     @Test
     void equals_shouldReturnFalseForNotEqualNames() {
-        long id = 123;
-        ApiResponseOrganization apiResponseOrganization1 = new ApiResponseOrganization(id, "testname", "testhomepage"
+        long id1 = 123;
+        long id2 = 456;
+        ApiResponseOrganization apiResponseOrganization1 = new ApiResponseOrganization(id1, "testname1", "testhomepage"
                 , "testmail", new ApiResponseAddress("testStreet", new ApiResponseAddressDetails("11", "Berlin")));
-        ApiResponseOrganization apiResponseOrganization2 = new ApiResponseOrganization(id, "testname2", "testhomepage"
+        ApiResponseOrganization apiResponseOrganization2 = new ApiResponseOrganization(id2, "testname2", "testhomepage"
                 , "testmail", new ApiResponseAddress("testStreet", new ApiResponseAddressDetails("11", "Berlin")));
         assertNotEquals(apiResponseOrganization1, apiResponseOrganization2);
     }

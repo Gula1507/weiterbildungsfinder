cbc6bab38b598a1ecbc934ece3f05896b5b0bb5f
Aygul <gulya@gmx.net>
2024-12-12 13:29:06 +0100

    feat: add author and id to review model and type

--- backend/src/main/java/de/neuefische/backend/model/Review.java
+++ backend/src/main/java/de/neuefische/backend/model/Review.java
@@ -3,6 +3,6 @@ package de.neuefische.backend.model;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 
-public record Review(String comment, @Min(1) @Max(5) int starNumber) {
+public record Review(String id, String author, String comment, @Min(1) @Max(5) int starNumber) {
 
 }

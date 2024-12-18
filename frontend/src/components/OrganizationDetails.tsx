ba51dc8cfe3f9d1f31682613ceca43bdae65507d
Aygul <gulya@gmx.net>
2024-12-18 12:39:10 +0100

    feat: show courses and course details on organization details page; - style course details

--- frontend/src/components/OrganizationDetails.tsx
+++ frontend/src/components/OrganizationDetails.tsx
@@ -6,6 +6,7 @@ import "../styles/OrganizationDetails.css"
 import StarsRating from "./StarsRating.tsx";
 import {AppUser} from "../types/AppUser.ts";
 
+
 type OrganizationDetailsProps = {
     appUser: AppUser|null|undefined;
     onLogin: (user: AppUser) => void;
@@ -19,6 +20,7 @@ function OrganizationDetails(props:OrganizationDetailsProps) {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
     const [deleteSuccess, setDeleteSuccess] = useState(false);
+    const [expandedCourseId, setExpandedCourseId] = useState<number | null>(null);
 
     useEffect(() => {
 
@@ -59,6 +61,11 @@ function OrganizationDetails(props:OrganizationDetailsProps) {
             });
     }
 
+
+    const toggleCourseDetails = (courseId: number) => {
+        // Toggle der Anzeige der Kursdetails
+        setExpandedCourseId(prevId => (prevId === courseId ? null : courseId));
+    };
     return (
         <div>
 
@@ -108,12 +115,12 @@ function OrganizationDetails(props:OrganizationDetailsProps) {
             <div className="spacing">
                 <strong>Durchschnitsnote: </strong>
                 {(organization?.averageRating === null || organization?.averageRating === 0.0)
-                ? "noch keine Bewertung"
+                    ? "noch keine Bewertung"
 
-                : (<StarsRating rating={parseFloat(organization?.averageRating.toFixed(1) as string)}/>)}
+                    : (<StarsRating rating={parseFloat(organization?.averageRating.toFixed(1) as string)}/>)}
             </div>
 
-            <div><strong>Rezensionen: </strong> {organization?.reviews && organization.reviews.length > 0
+            <div className="spacing"><strong>Rezensionen: </strong> {organization?.reviews && organization.reviews.length > 0
                 ?
                 (<ul className="review-list">
                     {organization?.reviews.map((r, index) => (
@@ -122,6 +129,46 @@ function OrganizationDetails(props:OrganizationDetailsProps) {
                         </li>))}
                 </ul>)
                 : "noch keine Rezensionen vorhanden"}</div>
+            <div className="spacing">
+                <strong> Kurse: </strong>
+                {organization?.courses && organization.courses.length > 0 ? (
+                    <ul className="course-list">
+                        {organization.courses.map((course) => (
+                            <li key={course.courseId} className="course-item">
+                                <div onClick={() => toggleCourseDetails(course.courseId)} style={{ cursor: 'pointer' }}>
+                                    <strong>{course.courseName}</strong>
+                                </div>
+
+                                {expandedCourseId === course.courseId && (
+                                    <div className="course-details">
+                                        <p className="title-course-details"><strong>Inhalt:</strong></p>
+                                        <div
+                                            dangerouslySetInnerHTML={{__html: course.courseContent}}
+                                            className="course-element"
+                                        />
+                                        <p className="title-course-details"><strong>Abschluss:</strong></p>
+                                        <div
+                                            dangerouslySetInnerHTML={{__html: course.courseDegree}}
+                                            className="course-element"
+                                        />
+                                        <p className="title-course-details">
+                                            <strong>Förderung:</strong></p>
+                                        {course.educationVoucher ?(
+                                        <div
+                                            dangerouslySetInnerHTML={{__html: course.educationVoucher}}
+                                            className="course-element"
+                                        />)
+                                           : "Keine Informationen"}
+
+                                    </div>
+                                )}
+                            </li>
+                        ))}
+                    </ul>
+                ) : (
+                    "Keine Kurse vorhanden"
+                )}
+            </div>
         </div>
     );
 }
